ebc395df89d77a5970f972a0603e810879299e8c
gunjit <73329409+GUNJITDALAL@users.noreply.github.com>
2021-01-27 23:16:13 +0530

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,13 @@
+// Write a program print a table of number 2
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num=1;
+	do	
+	{
+		printf("%d\n",2*num);
+		num++;		
+	}while(num<=10);
+	return 0;
+}
